1e5861289adcce3dced824aa63bf85c6fe2a0427
Margarita Ostrovskaia <33210089+MargaritaOstrovskaia@users.noreply.github.com>
2019-10-08 15:15:09 -0700

    changes

--- get_next_line.h
+++ get_next_line.h
@@ -1,7 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mostrovs <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/08 13:31:30 by mostrovs          #+#    #+#             */
+/*   Updated: 2019/10/08 13:31:36 by mostrovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 4
+# define BUFF_SIZE 8
 # define FD_SIZE 4096
 
 # include "libft/libft.h"
